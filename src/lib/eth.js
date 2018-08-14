be93f49361a37266e66f7dab7c5acf51187ce1bd
Tal Rasha <talrasha007@gmail.com>
2018-08-15 03:14:45 +0800

    update eth

--- src/lib/eth.js
+++ src/lib/eth.js
@@ -2,7 +2,8 @@ import { web3 } from 'web3-api-wrap'
 
 const addresses = require('../../coincow-contracts/build/contract_addresses.json')
 const userInfoAbi = require('../../coincow-contracts/build/contracts/UserInfo.json').abi
-const TestCowAbi = require('../../coincow-contracts/build/contracts/TestCow.json').abi
+const BtcSwapCow = require('../../coincow-contracts/build/contracts/BtcSwapCow.json').abi
+const EthSwapCow = require('../../coincow-contracts/build/contracts/EthSwapCow.json').abi
 const coinCowCoreAbi = require('../../coincow-contracts/build/contracts/CoinCowCore.json').abi
 const farmAbi = require('../../coincow-contracts/build/contracts/Farm.json').abi
 const auctionHouseAbi = require('../../coincow-contracts/build/contracts/AuctionHouse.json').abi
@@ -10,18 +11,18 @@ const auctionHouseAbi = require('../../coincow-contracts/build/contracts/Auction
 export { web3 }
 export const contracts = {
   userInfo: web3.loadContract(userInfoAbi, addresses.userInfo),
-  EtherCow: web3.loadContract(TestCowAbi, addresses.testEthCow),
-  BitcoinCow: web3.loadContract(TestCowAbi, addresses.testBtcCow),
+  EtherCow: web3.loadContract(EthSwapCow, addresses.ethSwapCow),
+  BitcoinCow: web3.loadContract(BtcSwapCow, addresses.btcSwapCow),
   coinCowCore: web3.loadContract(coinCowCoreAbi, addresses.coinCowCore),
   farm: web3.loadContract(farmAbi, addresses.farm),
   auctionHouse: web3.loadContract(auctionHouseAbi, addresses.auctionHouse)
 }
 export const coinMap = {
-  [addresses.testEthCow]: {
+  [addresses.ethSwapCow]: {
     type: 'ether',
     contract: contracts.EtherCow
   },
-  [addresses.testBtcCow]: {
+  [addresses.btcSwapCow]: {
     type: 'bitcoin',
     contract: contracts.BitcoinCow
   }
